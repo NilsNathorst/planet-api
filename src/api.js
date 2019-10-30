19729543458defabd93a36cb2495c515a0703f17
Nils <nilsnathorstwindahl@gmail.com>
2019-10-30 19:28:24 +0100

    pleaseassaeasdasdas

--- src/api.js
+++ src/api.js
@@ -8,8 +8,8 @@ firebase.initializeApp({
   credential: firebase.credential.cert({
     type: "service_account",
     project_id: "only-one-planet",
-    private_key_id: JSON.parse(process.env.FB_PRIVATE_KEY_ID),
-    private_key: JSON.parse(process.env.FB_PRIVATE_KEY),
+    private_key_id: process.env.FB_PRIVATE_KEY_ID,
+    private_key: process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n"),
     client_email:
       "firebase-adminsdk-bgs28@only-one-planet.iam.gserviceaccount.com",
     client_id: "105516695166122801845",
