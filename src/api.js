49cdff516a2a5b85530562dec7ecc8116d802bd1
Nils <nilsnathorstwindahl@gmail.com>
2019-10-30 19:06:29 +0100

    updated meth

--- src/api.js
+++ src/api.js
@@ -3,27 +3,23 @@ const serverless = require("serverless-http");
 require("dotenv").config();
 const app = express();
 const router = express.Router();
-
 var firebase = require("firebase-admin");
-const val = process.env.PRIVATE_KEY;
-console.log(val);
-const serviceAccount = {
-  type: "service_account",
-  project_id: "only-one-planet",
-  private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email:
-    "firebase-adminsdk-bgs28@only-one-planet.iam.gserviceaccount.com",
-  client_id: "105516695166122801845",
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-bgs28%40only-one-planet.iam.gserviceaccount.com"
-};
 
 firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
+  credential: firebase.credential.cert({
+    type: "service_account",
+    project_id: "only-one-planet",
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email:
+      "firebase-adminsdk-bgs28@only-one-planet.iam.gserviceaccount.com",
+    client_id: "105516695166122801845",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url:
+      "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-bgs28%40only-one-planet.iam.gserviceaccount.com"
+  }),
   databaseURL: "https://only-one-planet.firebaseio.com"
 });
 
